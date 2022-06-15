e07c09a8ab90427b4cfd318b1fa2835dee4d5697
Iskandar <30454668+Iskandar-S@users.noreply.github.com>
2022-06-15 11:23:18 -0400

    created directory for wk3 day2, reverseString.js for leetcode practice

---
+++ Mod1/wk3/day2/reverseString.js
@@ -0,0 +1,31 @@
+//! Write a function that reverses a string. The input string is given as an array of characters s.
+
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+//  Do not return anything, modify s in-place instead.
+
+// Example:
+
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+
+
+let s = ['h', 'e', 'l', 'l', 'o'];
+
+var reverseString = function (s) {
+  let i = 0;
+  let j = s.length - 1;
+
+  while (i < j) {
+    let temp = s[i];
+    s[i] = s[j];
+    s[j] = temp;
+
+    i++;
+    j--;
+  }
+};
+
+reverseString(s);
+
+console.log(s);
