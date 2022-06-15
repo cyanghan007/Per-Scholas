3719b8aa1145f8a2a66d903e703e090dc671e164
Iskandar <30454668+Iskandar-S@users.noreply.github.com>
2022-06-15 11:52:22 -0400

    accessed multiple elements, iterated over a collection of elements

--- Mod1/wk3/day2/script.js
+++ Mod1/wk3/day2/script.js
@@ -1,6 +1,6 @@
-let titleE1 = document.getElementById('title');
-titleE1.style.textAlign = 'center';
-console.log(titleE1);
+let titleEl = document.getElementById('title');
+titleEl.style.textAlign = 'center';
+console.log(titleEl);
 
 let titleOther = document.querySelector('#title');
 console.log(titleOther);
@@ -8,12 +8,23 @@ console.log(titleOther);
 let myChoice = document.querySelector('h1');
 console.log(myChoice);
 
-let pE1 = document.querySelector('.cool');
-pE1.style.color = 'blue';
-console.log(pE1);
+let pEl = document.querySelector('.cool');
+pEl.style.color = 'blue';
+console.log(pEl);
 
-pE1.innerHTML = "Comments for <strong>Today";
+pEl.innerHTML = "Comments for <strong>Today";
 
-let aE1 = document.querySelector('#google-link');
-aE1.setAttribute('href', "https://www.google.com");
-console.log(aE1);
+let aEl = document.querySelector('#google-link');
+aEl.setAttribute('href', "https://www.google.com");
+console.log(aEl);
+
+let commentEls = document.querySelectorAll('.comment');
+console.log(commentEls);
+
+for(commentEl of commentEls) {
+  console.log(commentEl);
+}
+
+for(commentEl of commentEls) {
+  commentEl.style.fontSize = '30px';
+}
