7f33eb0310fdfcad540fe75c2ecbb2a1497629ed
Iskandar <30454668+Iskandar-S@users.noreply.github.com>
2022-06-15 11:25:04 -0400

    created index.html, script.js for DOM practice

---
+++ Mod1/wk3/day2/script.js
@@ -0,0 +1,19 @@
+let titleE1 = document.getElementById('title');
+titleE1.style.textAlign = 'center';
+console.log(titleE1);
+
+let titleOther = document.querySelector('#title');
+console.log(titleOther);
+
+let myChoice = document.querySelector('h1');
+console.log(myChoice);
+
+let pE1 = document.querySelector('.cool');
+pE1.style.color = 'blue';
+console.log(pE1);
+
+pE1.innerHTML = "Comments for <strong>Today";
+
+let aE1 = document.querySelector('#google-link');
+aE1.setAttribute('href', "https://www.google.com");
+console.log(aE1);
