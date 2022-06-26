d21e0f3de2d34324866c4f4fd764489c4f0629ca
Iskandar <30454668+Iskandar-S@users.noreply.github.com>
2022-06-25 23:22:20 -0400

    created class Spaceship

---
+++ Space_Battle_OOP_Lab/script.js
@@ -0,0 +1,11 @@
+class Spaceship {
+  constructor(type, hull, firepower, accuracy) {
+    this.type = type;
+    this.hull = hull;
+    this.firepower = firepower;
+    this.accuracy = accuracy;
+  }
+  attack() {
+    
+  }
+}
