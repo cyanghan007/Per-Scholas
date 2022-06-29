a8137aa27e90c2651d0af7f351647ad013e9e233
Iskandar <30454668+Iskandar-S@users.noreply.github.com>
2022-06-29 07:56:50 -0400

    renamed js file

--- Space_Battle_OOP_Lab/app.js
+++ Space_Battle_OOP_Lab/app.js
@@ -3,6 +3,7 @@ const enemyHull = document.querySelector('#enemyHull');
 const enemyFirepower = document.querySelector('#enemyFirepower');
 const enemyAccuracy = document.querySelector('#enemyAccuracy');
 
+// class Spaceship to create player and alien spaceship objects
 class Spaceship {
   constructor(type, hull, firepower, accuracy) {
     this.type = type;
@@ -10,10 +11,11 @@ class Spaceship {
     this.firepower = firepower;
     this.accuracy = accuracy;
   }
+  // async method to attack enemy
   async attack(enemy) {
     if (Math.random() < this.accuracy) {
       enemy.hull -= this.firepower;
-      await timeout(1500)
+      await timeout(1500) //timeout to slow down the console and achieve smoother gameplay
       console.log(`+${this.firepower} Damage`);
     } else {
       await timeout(1500)
@@ -22,9 +24,10 @@ class Spaceship {
   }
 }
 
+// async function for a round
 const round = async (player, alien) => {
   while(player.hull > 0 && alien.hull > 0) {
-    updateDOM(player, alien);
+    updateDOM(player, alien); //updating DOM to reflect stats of the player and alien spaceships
     await timeout(1500)
     console.log(`Attack!`);
     await player.attack(alien);
@@ -40,19 +43,24 @@ const round = async (player, alien) => {
   await timeout(1500);
 };
 
+// human spaceship
 const player = new Spaceship('human', 20, 5, 0.7);
 
+//array of alien spaceship parameters
 const alienHull = [3, 4, 5, 6];
 const alienFirepower = [2, 3, 4];
 const alienAccuracy = [0.6, 0.7, 0.8];
 
+// randomizer
 const random = array => Math.floor(Math.random() * array.length);
 
+//function to create random alien from array of parameters
 const randomAlien = () => {
   let alien = new Spaceship('alien', alienHull[random(alienHull)], alienFirepower[random(alienFirepower)], alienAccuracy[random(alienAccuracy)]);
   return alien;
 }
 
+//function to update stats
 const updateDOM = (player, enemy) => {
   playerHull.innerHTML = player.hull;
   enemyHull.innerHTML = enemy.hull;
@@ -60,31 +68,33 @@ const updateDOM = (player, enemy) => {
   enemyAccuracy.innerHTML = enemy.accuracy;
 }
 
+// async game function with rounds parameter
 const game = async (rounds) => {
-  let i = 1;
+  let i = 1; //rounds counter
   for (i; i <= rounds; i++) {
     await timeout(1500)
-    console.log(`Round ${i}`);
-    await round(player, randomAlien());
-    if (player.hull > 0) {
+    console.log(`Round ${i}`); //declaring round
+    await round(player, randomAlien()); //awaiting for round results
+    if (player.hull > 0) { //check if human spaceship has not been destroyed
       console.log('Alien Spaceship destroyed!');
-      if (i === rounds) {
+      if (i === rounds) { //check if rounds played is equal to rounds argument
         console.log('You won!');
-        break;
+        break; //breaking the loop
       }
-    } else {
+    } else { //if human spaceship has been destroyed
       console.log('You lost!');
       break;
     }
     await timeout(1500);
-    let attack = prompt('Attack [1] or Retreat [2]?', 1)
-    if (attack != 1) {
+    let attack = prompt('Attack [1] or Retreat [2]?', 1) //prompt to continue. Located at the end of function to run first round by default
+    if (attack != 1) { //checking input
       console.log('GAME OVER!');
       break;
     }
   }
 }
 
+// timeout promise to slow down the game returns an empty promise
 function timeout (ms) {
   return new Promise(resolve => {
     setTimeout(() => {
@@ -93,4 +103,5 @@ function timeout (ms) {
   })
 }
 
+// running the game
 game(6);
