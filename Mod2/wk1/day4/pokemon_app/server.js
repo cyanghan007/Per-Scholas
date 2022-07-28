9939c59d2f9a46c0f47d184ed8753c3e9d678259
Iskandar <30454668+Iskandar-S@users.noreply.github.com>
2022-07-28 15:40:09 -0400

    index.jsx view rendered at pokemon route

--- Mod2/wk1/day4/pokemon_app/server.js
+++ Mod2/wk1/day4/pokemon_app/server.js
@@ -4,13 +4,16 @@ require('dotenv').config();
 const port = process.env.PORT || 3003;
 const pokemon = require('./models/pokemon');
 
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
 app.get('/', (req, res) => {
   res.send('Welcome to the Pokemon App!');
 });
 
 app.get('/pokemon', (req, res) => {
-  res.send(pokemon);
-})
+  res.render('Index');
+});
 
 app.listen(port, () => {
   console.log('Express server listening on ', port);
