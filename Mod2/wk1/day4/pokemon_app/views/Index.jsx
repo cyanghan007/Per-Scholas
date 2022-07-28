15f3221f1828611e636988478b4ce20bf36fb6be
Iskandar <30454668+Iskandar-S@users.noreply.github.com>
2022-07-28 16:41:59 -0400

    Added dynamic anchor tags to index.jsx

--- Mod2/wk1/day4/pokemon_app/views/Index.jsx
+++ Mod2/wk1/day4/pokemon_app/views/Index.jsx
@@ -16,7 +16,9 @@ class Index extends React.Component {
                 {pokemon.map((pokemon, i) => {
                   return (
                     <li>
-                      {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+                      <a href={`/pokemon/${i}`}>
+                        {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+                      </a>
                     </li>
                   );
                 })}
