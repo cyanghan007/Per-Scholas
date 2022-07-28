75b324758eb26bcdeff39feae98b4bac02efe2b2
Iskandar <30454668+Iskandar-S@users.noreply.github.com>
2022-07-28 18:07:31 -0400

    Tip Calculator express application created.

--- Mod2/wk1/day3/homework/server.js
+++ Mod2/wk1/day3/homework/server.js
@@ -2,13 +2,20 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3003;
 
+// greeting without name parameter
 app.get('/greeting', (req, res) => {
   res.send(`Hello, stranger`);
-})
+});
 
+// greeting with name parameter
 app.get('/greeting/:name', (req, res) => {
   res.send(`Hello, ${req.params.name}`);
-})
+});
+
+// tip calculator
+app.get('/tip/:total/:tip', (req, res) => {
+  res.send(`${req.params.total * (req.params.tip / 100)}`)
+});
 
 app.listen(port, () => {
   console.log('listening on port ', port)
