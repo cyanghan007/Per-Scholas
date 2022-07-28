c753c791856a87cddb5d946b57e9174f54182aab
Iskandar <30454668+Iskandar-S@users.noreply.github.com>
2022-07-28 18:35:34 -0400

    Magic 8 Ball express application created.

--- Mod2/wk1/day3/homework/server.js
+++ Mod2/wk1/day3/homework/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3003;
+const response = require('./models/response.js');
 
 // greeting without name parameter
 app.get('/greeting', (req, res) => {
@@ -15,6 +16,12 @@ app.get('/greeting/:name', (req, res) => {
 // tip calculator
 app.get('/tip/:total/:tip', (req, res) => {
   res.send(`${req.params.total * (req.params.tip / 100)}`)
+});
+
+// magic 8 ball
+app.get('/magic/:question', (req, res) => {
+  res.send(`${req.params.question}?
+  <h1>${response[Math.floor(Math.random() * response.length)]}</h1>`);
 });
 
 app.listen(port, () => {
