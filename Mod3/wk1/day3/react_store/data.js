74c0009fc3485f887332a553c3ca6202095305b6
Iskandar <30454668+Iskandar-S@users.noreply.github.com>
2022-08-19 13:14:14 -0400

    finished day 3

---
+++ Mod3/wk1/day3/react_store/data.js
@@ -0,0 +1,40 @@
+const products = [
+	{ name: 'allen wrench', price: 2.99, description: 'handy tool' },
+	{
+		name: 'cornucopia',
+		price: 5.99,
+		description: 'festive holiday decoration',
+	},
+	{ name: 'banana', price: 0.99, description: 'full of potassium' },
+	{
+		name: 'guillotine(cigar)',
+		price: 10.59,
+		description: 'behead your stub',
+	},
+	{
+		name: 'jack-o-lantern',
+		price: 3.99,
+		description: 'spooky seasonal fun',
+	},
+	{
+		name: 'doggie treat(box)',
+		price: 5.99,
+		description: 'Fido loves em',
+	},
+	{
+		name: 'egg separator',
+		price: 3.99,
+		description: 'it separates yolks from the whites',
+	},
+	{
+		name: 'LED lightbulb',
+		price: 6.55,
+		description: 'Super efficient!',
+	},
+	{
+		name: 'owl pellets',
+		price: 3.99,
+		description: 'do NOT ask what these used to be...',
+	},
+	{ name: 'flag', price: 5.99, description: 'catches the breeze' },
+];
